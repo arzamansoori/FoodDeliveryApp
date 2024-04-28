bb67831ba0c3c4f6a4f0d7db0722a9113fc3309a
arzatf <arzamansoori786@gmail.com>
2024-04-28 21:30:08 +0530

    adding routes to app

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <div>
+            <h2>About us</h2>
+            <p>We're food delivering app.</p>
+        </div>
+    )
+}
+
+export default About;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,15 @@
 import { LOGO_URL } from "../utils/constants";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 
 const Header = () => {
   const [btnNameReact, setBtnNameReact] = useState("Login");
+
+  console.log("header rendered")
+
+  //useEffect uses
+  useEffect(()=>{
+    console.log("useEffect called")
+  },[btnNameReact])
 
   return (
     <div className="heading">
