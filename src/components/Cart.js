641f84cbd0279dfc8c1460c5dd425efef1088d79
arzatf <arzamansoori786@gmail.com>
2024-08-12 23:30:12 +0530

    added functionality of clear cart and total bill

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,6 +1,6 @@
-import { useSelector } from "react-redux";
-import ResItemList from "./ResItemList";
-import { CDN_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { clearCart } from "../reduxComponents/cartSlice";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
   const cartItems = useSelector((store) => store.cart.items);
@@ -15,17 +15,26 @@ const Cart = () => {
   console.log("cartItems post price", cartItems);
   console.log("itemprice", itemPrice);
 
-  let totalBill = Object.values(itemPrice).reduce((acc, cv) => {
-    return acc + cv
-  })
+  const dispatch = useDispatch();
 
-  console.log("TotalBill", totalBill)
+  const handleClearCart = () => {
+    dispatch(clearCart());
+  };
+
+  let totalBill = Object.values(itemPrice).reduce((acc, cv) => {
+    return acc + cv;
+  }, 0);
+
+  console.log("TotalBill", totalBill);
 
   return (
     <div className="space-y-7">
       <div className="flex flex-col m-3 items-center">
         <div className="m-1 p-2 font-medium text-lg">CART</div>
-        <button className="text-red-800 bg-slate-100 font-medium border border-black rounded-lg p-1">
+        <button
+          className="text-red-800 bg-slate-100 font-medium border border-black rounded-lg p-1"
+          onClick={handleClearCart}
+        >
           Clear Cart
         </button>
       </div>
@@ -37,10 +46,7 @@ const Cart = () => {
               <div className="">
                 <div className="mx-28 px-8 flex justify-between py-2 bg-slate-100 rounded-md border-gray-200 border-b-2 flex-wrap">
                   <span className="font-normal ">{item.card.info.name}</span>
-                  <span className="">
-                    ₹
-                    {itemPrice[item.card.info.id]}
-                  </span>
+                  <span className="">₹{itemPrice[item.card.info.id]}</span>
                 </div>
               </div>
             </div>
@@ -49,8 +55,11 @@ const Cart = () => {
       </div>
 
       <div className="flex justify-center">
-        <span className="font-bold text-xl">
-          Total Bill - {totalBill}
+        <span className="">
+          {cartItems.length == 0
+            ? <p>Ooops! No items in the cart. Go to <Link className="text-blue-700 font-mono border-b-2 border-blue-600" to="/">Home</Link> to add items of your choice.</p>
+            : <p className="font-bold text-xl">Total Bill - { totalBill }</p>
+            }
         </span>
       </div>
     </div>
