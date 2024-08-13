6d586d440a3f9c680fdea6522038b3fa087693ce
arzatf <arzamansoori786@gmail.com>
2024-08-13 09:21:20 +0530

    corrected error message added link to home page

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,14 +1,19 @@
-import { useRouteError } from "react-router-dom"
+import { Link, useRouteError } from "react-router-dom";
 
 const Error = () => {
-    const err = useRouteError();
-    console.log(err);
-    return (
-        <div>
-            <h3>Ooops! Something went wrong</h3>
-            <h4>{err.status}: {err.statusText}</h4>
-        </div>
-    )
-}
+  const err = useRouteError();
+  console.log(err);
+  return (
+    <div className="flex flex-col items-center mt-20">
+      <h3>Ooops! Something went wrong</h3>
+      <h4>
+        {err.status}: {err.statusText}
+      </h4>
+      <button className="bg-pink-200 p-1 m-1 border-black border-2 rounded-lg font-mono">
+        <Link to="/">Go to Home</Link>
+      </button>
+    </div>
+  );
+};
 
 export default Error;
